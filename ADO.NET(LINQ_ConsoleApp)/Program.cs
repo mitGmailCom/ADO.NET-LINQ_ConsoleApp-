0098d132160815880f4396894081a39beb78c4a8
Admin <Admin@X>
2018-07-17 10:59:13 +0300

    Добавьте файлы проекта.

---
+++ ADO.NET(LINQ_ConsoleApp)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_LINQ_ConsoleApp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
